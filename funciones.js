51d575c984e43322c04c800a78b0c23b69c2c454
Francina Zuliani <2002300.ucc.edu.ar>
2021-06-14 21:28:54 -0300

    .

---
+++ funciones.js
@@ -0,0 +1,43 @@
+function nombre(id,valor)
+{
+        if (!isNaN(valor))
+        {
+            alert("Por favor ingrese solo letras");
+            document.nombrejug.jugadorn1.value = "";
+        }
+}
+function nombre2(id,valor)
+{
+    if (!isNaN(valor))
+    {
+        alert("Por favor ingrese solo letras");
+        document.nombrejug.jugadorn2.value = "";
+    }
+}
+function palabra (id,valor)
+{
+    if (!isNaN(valor))
+    {
+        alert("Por favor ingrese solo letras");
+        document.ip.ingpalabra.value= "";
+    }
+}
+
+//const p= palab(variable donde se guarda la palabra a ingresar).replace(/./g,"-");
+const l(letra)=document.getElementsByName(introletra).value;
+
+/**function mostju ()
+{
+    var nomj;
+    nomj=document.getElementsByName(elementName: "jug1").value;
+}**/
+
+
+
+
+/**function nomjug1 ()
+{
+    var canvas = canvas.getElementById(elementld:"nombrejug");
+    var ctx = canvas.getContext("2d");
+    var n1= document.getElementById(elementld:"inpj1");
+}**/
