c531d07784e06eaa020a0930434779398c3142be
Francina Zuliani <2002300.ucc.edu.ar>
2021-06-15 23:09:50 -0300

    actualización

--- funciones.js
+++ funciones.js
@@ -1,3 +1,9 @@
+
+addEventListener('load',f);
+
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length); }
+
 function nombre(id,valor)
 {
         if (!isNaN(valor))
@@ -6,38 +12,62 @@ function nombre(id,valor)
             document.nombrejug.jugadorn1.value = "";
         }
 }
-function nombre2(id,valor)
-{
-    if (!isNaN(valor))
+const palabras=['exterior', 'lamentar', 'sombra', 'germen', 'adrenalina', 'almendra', 'rescate', 'principales',
+    'encontramos', 'azul', 'amarillo', 'importantes', 'habitaciones', 'laboratorios', 'nacionalidad', 'laberintos',
+    'calculadora', 'enchufe', 'celular', 'estufa', 'sol', 'sal', 'heladera','tostador','computadora','leche','agua',
+    'teclado','estudiar', 'tarea','electrodomestico', 'azulejos', 'mermelada', 'helado', 'hamaca', 'almohadon',
+    'exhibir', 'cuchillo', 'bostezar', 'tramite', 'dormir'];
+const palabra= palabras[Math.floor(Math.random()*palabras.length)];
+let palabraConGuiones = palabra.replace(/./g, "_ ");
+
+document.querySelector('#output').innerHTML = palabraConGuiones;
+function f() {
+    let contadorfallo=0;
+    document.querySelector('#output').innerHTML=palabraConGuiones;
+    document.querySelector('#completra').addEventListener('mousedown',a=>
     {
-        alert("Por favor ingrese solo letras");
-        document.nombrejug.jugadorn2.value = "";
-    }
+        let fallo=true;
+        const letra = document.querySelector('#ingletra').value;
+        if (!isNaN(letra))
+        {
+            alert("Por favor ingrese solo letras");
+            document.querySelector('#ingletra').value="";
+        }
+
+        for(const i in palabra){
+            if(letra==palabra[i]){
+                palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra);
+                fallo=false;
+            }
+        }
+        if(fallo){
+            contadorfallo++;
+            cc();
+            document.querySelector('#avocados').style.backgroundPosition=-(130*contadorfallo)+'px 0';
+            if (contadorfallo==7){
+                alert("Perdiste el juego :( ")
+            }
+        }else{
+            if(palabraConGuiones.indexOf('_')<0){
+                alert("Ganaste el juego :)")
+            }
+        }
+        document.querySelector('#output').innerHTML=palabraConGuiones;
+        document.querySelector('#ingletra').value="";
+    });
+
 }
-function palabra (id,valor)
-{
-    if (!isNaN(valor))
-    {
-        alert("Por favor ingrese solo letras");
-        document.ip.ingpalabra.value= "";
+
+function cc () {
+    var canvas = document.getElementById("puntaje");
+    puntaje = canvas.getContext("2d");
+    var ii = new Image();
+    ii.scr = "Imágenes/delete-156119_960_720.png";
+    ii.onload = function () {
+        ctx.drawImage(img, 0, 0);
     }
+    ii.addEventListener("load", function () {
+        puntaje.drawImage(ii, 20, 20)
+    }, false);
 }
 
-//const p= palab(variable donde se guarda la palabra a ingresar).replace(/./g,"-");
-const l(letra)=document.getElementsByName(introletra).value;
-
-/**function mostju ()
-{
-    var nomj;
-    nomj=document.getElementsByName(elementName: "jug1").value;
-}**/
-
-
-
-
-/**function nomjug1 ()
-{
-    var canvas = canvas.getElementById(elementld:"nombrejug");
-    var ctx = canvas.getContext("2d");
-    var n1= document.getElementById(elementld:"inpj1");
-}**/
